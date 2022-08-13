b559c25e5dd549edcc1a0dadc54e8193091332a6
zahid99 <zahid.cse07@gmail.com>
2022-08-13 22:08:45 +0600

    Add project files.

---
+++ WebApiFileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalApiFileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult FileUpload(IFormFile file)
+        {
+            var uploadPath = Path.Combine(@"D:\");
+
+            if (file.FileName.Length > 0)
+            {
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(uploadPath, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok("File Upload Successfully");
+            }
+
+            return BadRequest();
+
+        }
+    }
+}
